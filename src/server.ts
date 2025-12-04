368347c60938bd74d638843b12dbff90de254ed9
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-04 15:07:59 -0300

    Melhorando o backend

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
@@ -7,10 +8,13 @@ const port = 3000;
 // Middleware para JSON
 app.use(express.json());
 
+// Habilita CORS (permite requisições do frontend em outra porta)
+app.use(cors());
+
 // Usa as rotas definidas
 app.use(router);
 
 // Inicia o servidor
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}/users`);
 });
